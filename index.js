f771a3e176128dd6dd345405a54cdeeea8ea1b02
Gray MacGregor <gray.mcgregor@travelex.com>
2017-02-14 22:08:23 +0000

    new dashboard

--- index.js
+++ index.js
@@ -2,69 +2,90 @@ var blessed = require('blessed');
 var contrib = require('blessed-contrib');
 var _ = require('lodash');
 var moment = require('moment');
-
-var jenkinsUrl = process.env.JENKINS_URL || 'YOUR_JENKINS_SERVER';
-jenkinsUrl = 'http://' + jenkinsUrl;
-
-var jobsFilterRegex = new RegExp(process.env.JOBS_FILTER, 'i');
-
+var colors = require('colors/safe');
+var jenkinsUrl = process.env.JENKINS_URL;
+if (!jenkinsUrl) {
+    throw new Error('JENKINS_URL not set');
+}
 var jenkins = require('jenkins')(jenkinsUrl);
-
+var jobsFilterRegex = new RegExp(process.env.JOBS_FILTER, 'i');
 var screen = blessed.screen();
+var grid = new contrib.grid({rows: 14, cols: 12, screen: screen});
+var cols = 3;
 
-var grid = new contrib.grid({rows: 13, cols: 12, screen: screen})
-
-var defaultStyle = { fg: 'white', bg: 'black', border: { fg: '#f0f0f0' } };
-
-var addJobToGrid = function (job, grid, x, y, w, h) {
-
-	var box = grid.set(x, y, w, h, blessed.box, { content: job.name, style: defaultStyle } );
-	box.style.bg = job.color.split('_')[0];
-
-	if(job.color.split('_')[1] === 'anime') {
-
-		setTimeout(function() {
-			box.style.bg = '#999999';
-			box.style.fg = 'black';
-			screen.render();
-		}, 1000);
-
-	}
-
+function filterJobs(jobs) {
+    return _.filter(jobs, function (job) {
+        return job.name.match(jobsFilterRegex);
+    });
 };
 
-var drawGrid = function (buildStatus, callback) {
+function getJobColor(job) {
+    if (black && /_anime/.test(job.color)) {
+	return colors.black(job.name);
+    }
 
-	var box = grid.set(0, 4, 4, 4, blessed.box, { content: moment().format('YYYY-MM-DD HH:mm:ss'), style: defaultStyle } );
+    var jobColor = job.color.replace('_anime', '');
+    var colorMap = {
+        aborted: 'orange',
+        blue: 'green',
+        disabled: 'gray',
+        grey: 'gray',
+        notbuilt: 'gray',
+        red: 'red',
+        yellow: 'yellow'
+    };
+    var color = colorMap.hasOwnProperty(jobColor) ? colorMap[jobColor] : 'yellow';
+    if (colors[color]) {
+	return colors[color](job.name);
+    } else {
+	return job.name;
+    }
+}
 
-	addJobToGrid(buildStatus[0], grid, 1, 0, 6, 4);
-	addJobToGrid(buildStatus[1], grid, 1, 4, 6, 4);
-	addJobToGrid(buildStatus[2], grid, 1, 8, 6, 4);
-	addJobToGrid(buildStatus[3], grid, 7, 0, 6, 4);
-	addJobToGrid(buildStatus[4], grid, 7, 4, 6, 4);
-	addJobToGrid(buildStatus[5], grid, 7, 8, 6, 4);
+function getJenkinsStatus(err, callback) {
+    if (err) {
+        return callback(err, null, getJenkinsStatus);
+    }
 
-	screen.render();
-
-	setTimeout(function() {
-		return callback(drawGrid);
-	}, 2000)
+    jenkins.info(function (error, data) {
+        if (error) {
+            return callback(error, null, getJenkinsStatus);
+        }
+        return callback(null, _.sortBy(filterJobs(data.jobs), 'name'), getJenkinsStatus);
+    });
 };
+var black = false;
+function showJobs(err, res, cb) {
+    var table = contrib.table({
+        keys: false,
+        fg: 'white',
+        interactive: false,
+        width: '100%',
+        height: '100%',
+        columnWidth: _.fill(_.range(cols), 28)
+    });
 
-var filterJobs = function (jobs) {
-	return _.filter(jobs, function (job) {
-		return job.name.match(jobsFilterRegex);
-	});
-};
+    screen.append(table);
 
-var getJenkinsStatus = function (callback) {
-	jenkins.info(function(err, data) {
-		 if (err) {
-			console.log(err);
-		 }
+    var jobs = _.map(res, getJobColor);
 
-		 return callback(_.sortBy(filterJobs(data.jobs), 'name'), getJenkinsStatus);
-	});
-};
+    table.setData({
+        headers: [moment().format('YYYY-MM-DD HH:mm:ss')],
+        data: _.chunk(jobs, cols),
+    });
 
-getJenkinsStatus(drawGrid)
+    screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+      return process.exit(0);
+    });
+
+    screen.render()
+
+    black = !black;
+
+    setTimeout(function () {
+        return cb(null, showJobs);
+    }, 1000);
+
+}
+
+getJenkinsStatus(null, showJobs);
